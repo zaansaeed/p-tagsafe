ae6f9bfd4598efaa243b6ce67dfb9583d6ddadb9
Zaan101 <77564853+Zaan101@users.noreply.github.com>
2025-12-02 21:37:22 -0800

    done

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Add this block below ⬇️
+  preview: {
+    allowedHosts: ["sunny-flow-production-b6eb.up.railway.app"],
+  },
 })
